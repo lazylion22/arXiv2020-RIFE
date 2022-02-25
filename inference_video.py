a8e9c96cbd782b54310565093ac342852fcd66ef
lazylion22 <88854657+lazylion22@users.noreply.github.com>
2022-02-25 18:48:37 +0200

    not working xd

--- inference_video.py
+++ inference_video.py
@@ -111,7 +111,7 @@ if not args.video is None:
     videoCapture.release()
     if args.fps is None:
         fpsNotAssigned = True
-        args.fps = fps 
+        args.fps = fps * (2 ** args.exp)
     else:
         fpsNotAssigned = False
     videogen = skvideo.io.vreader(args.video)
